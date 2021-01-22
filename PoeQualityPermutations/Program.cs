64a02a616b9f5fa64460820a8a386fb1d9d74cdb
TehGM <wojop95@gmail.com>
2021-01-22 10:19:22 +0100

    Add project files.

---
+++ PoeQualityPermutations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PoeQualityPermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
